b89374594ceace6f520a5ac5e12441bcd89c4c45
abrones <albrones1@gmail.com>
2023-10-29 18:33:08 +0100

    🧪(resume-line-detail): add detail for each mission using loop for better impl

---
+++ src/routes/ResumeLineDetail.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let description: string;
+	export let stack: string;
+</script>
+
+<div class="resume-line-detail">
+	<p>{description}</p>
+	<p>Stack: {stack}</p>
+</div>
+
+<style>
+	.resume-line-detail {
+		border: solid 0.07em gray;
+		margin: 4px;
+		padding: 0 1em;
+		display: flex;
+		flex-direction: column;
+		text-align: justify;
+	}
+	p {
+		width: 100%;
+	}
+</style>
